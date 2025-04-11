d44cfe5e688be1ffff9197aad9d5e5b89aa36c9d
Justina <justinamary27@gmail.com>
2025-04-11 21:51:30 +0530

    UI updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nomyxGreen: "#0fd795",
+        nomyxRed: "#EB5757",
+        nomyxBlue: "#3E81C8",
+        nomyxPurple: "#8383b7",
+        nomyxDarkPurple: "#974cd2",
+        nomyxLitePurple: "#c1a5ef",
+        nomyxDark1: "#111118",
+        nomyxDark2: "#232631",
+        nomyxGray1: "#BBBBBB",
+        nomyxGray2: "#878787",
+        nomyxGray3: "#626262",
+        nomyxGray4: "#484848",
+        nomyxWhite: "#F3F3F3",
+      },
+    },
   },
   plugins: [],
 };
