2cfc3ab7c25f47c0b2872f6681fcf2ea1267cc26
Syed Maisum Haider <maisum.haider@gmail.com>
2025-08-26 16:25:36 +0500

    fix missing webauthn update

--- src/services/DfnsService.ts
+++ src/services/DfnsService.ts
@@ -662,8 +662,7 @@ class DfnsService {
     }
 
     try {
-      const webauthn = new WebAuthnSigner();
-      const assertion = await webauthn.sign(challenge);
+      const assertion = await this.webauthn.sign(challenge);
 
       const completeResponse = await Parse.Cloud.run("dfnsSetFunctionClaimsComplete", {
         walletId,
